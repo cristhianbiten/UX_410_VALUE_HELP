f720983760084c50222a8377336ebe697e6b2a58
Cristhian Bitencourt <cristhianthiagok@yahoo.com>
2024-11-12 00:30:47 +0000

    first commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["student10/sap/training/valuehelp/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"student10saptraining/valuehelp/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
